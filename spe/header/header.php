37256a11146b0b7284bd8e917184be4369c1aad7
Dylan-txt <60511336+Dylan-txt@users.noreply.github.com>
2024-03-29 18:01:53 +0100

    header new

--- spe/header/header.php
+++ spe/header/header.php
@@ -4,9 +4,11 @@
             session_start();
             if (!empty($_SESSION['User'])) {
                 $user = $_SESSION['User'];
-                echo '<p>Bienvenue, ' . $user->loginName . '!</p>';
+                echo '    <a href="index.php" class="back-button">Menu</a>';
+               // echo '<p>Bienvenue, ' . $user->loginName . '!</p>';
                 echo '<a href="connexion/logout.php" class="logout-button">Déconnexion</a>';
             } else {
                 echo '<a href="connexion/connexion.html" class="login-button">Connexion</a>';
+                header('location:connexion/connexion.html');
             }
         ?>
